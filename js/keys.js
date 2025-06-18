2994129962f26a2568676c3c39996b52f502a521
gopes99 <gopes99@gmail.com>
2025-06-18 22:29:40 +0530

    Update keys.js

--- js/keys.js
+++ js/keys.js
@@ -1,7 +1,7 @@
 export const allowedCredentials = [
   {
     type: "public-key",
-    id: Uint8Array.from(atob("BASE64ENCODEDCREDENTIALID"), c => c.charCodeAt(0)),
+    id: Uint8Array.from(atob("i4lWVV6ph2bsAe7OWhoPuNIPujY="), c => c.charCodeAt(0)),
     transports: ["internal"]
   }
 ];
