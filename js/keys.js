f18123e9d29e2cc0703bd935f7985d45242bac3b
gopes99 <gopes99@gmail.com>
2025-06-18 23:05:24 +0530

    Update keys.js

--- js/keys.js
+++ js/keys.js
@@ -4,9 +4,5 @@ export const allowedCredentials = [
     id: Uint8Array.from(atob("i4lWVV6ph2bsAe7OWhoPuNIPujY="), c => c.charCodeAt(0)),
     transports: ["internal"]
   }
-    {
-    type: "public-key",
-    id: Uint8Array.from(atob("0ROJC+nos+nvygnz9y6YTHEeyQw="), c => c.charCodeAt(0)),
-    transports: ["internal"]
-  }
+   
 ];
